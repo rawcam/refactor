0a5af133467adec12b42178a3d0e1fe49b4a51ba
rawcam <rawcam@ya.ru>
2026-03-24 15:30:44 +0300

    Update tracts.js

--- js/tracts.js
+++ js/tracts.js
@@ -1,1 +1,18 @@
+// tracts.js
+const TractsModule = (function() {
+    let unsubscribe = null;
 
+    function init() {
+        console.log('TractsModule initialized (stub)');
+        // Подписка на изменения состояния для дальнейшей реализации
+        unsubscribe = AppState.subscribe((newState) => {
+            // Пока ничего не делаем
+        });
+    }
+
+    function destroy() {
+        if (unsubscribe) unsubscribe();
+    }
+
+    return { init, destroy };
+})();
